de54d40881ef8c88c3dbcea9becfb4ede0196b58
Yiannis Poulos <47044805+ypoulos96@users.noreply.github.com>
2019-03-01 00:15:51 -0600

    Add files via upload

---
+++ DVDLogo/DVDLogo/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DVDLogo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.update = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // update
+            // 
+            this.update.Enabled = true;
+            this.update.Interval = 25;
+            this.update.Tick += new System.EventHandler(this.update_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer update;
+    }
+}
+
---
+++ DVDLogo/DVDLogo/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVDLogo
+{
+    public partial class Form1 : Form
+    {
+        Image dvd = Image.FromFile("dvd_logo.jpg");
+        Random r = new Random();
+
+        private Color[] clrArr = { Color.Yellow, Color.Blue, Color.DeepPink, Color.Purple, Color.Orange };
+        private int x;
+        private int y;
+        private int xspeed = 5;
+        private int yspeed = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // Start image coordinates at random location
+            x = r.Next(0, this.Width - dvd.Width);
+            y = r.Next(0, this.Height - dvd.Height);           
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+           
+            g.DrawImage(dvd, x, y, 130, 90);    // Draw the image onto the form       
+        }
+
+        private void update_Tick(object sender, EventArgs e)
+        {            
+            // Move the image location every clock tick
+            x += xspeed;
+            y += yspeed;
+
+            // If a side of the image hits an edge of the form            
+            if (x <= 0 || x + 145 >= this.Width)
+            {
+                int i = r.Next(0, clrArr.Length);
+
+                xspeed *= -1;   // reverse the direction of the x-axis
+                this.BackColor = clrArr[i];     // Set background to random color
+            }
+
+            // If the top or bottom of the image hits the top or bottom of the form
+            if (y <= 0 || y + 125 >= this.Height)
+            {
+                int i = r.Next(0, clrArr.Length);
+
+                yspeed *= -1;   // Reverse the direction of the y-axis
+                this.BackColor = clrArr[i];     // Set background to random color
+            }
+
+            Invalidate();   // Update the frame
+        }
+    }
+}
